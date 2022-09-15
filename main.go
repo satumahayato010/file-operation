947966d95f23529c991810c4ca1bd095fb981578
stm33 <satuma33@icloud.com>
2022-09-16 08:32:28 +0900

    [add]refs #14 file-Read

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
-	"strings"
 )
 
 func main() {
-	read := strings.NewReader("Hello World!!!")
-	b, err := ioutil.ReadAll(read)
+	data := []byte("Hello World!!")
+	err := ioutil.WriteFile("test.txt", data, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(b))
 }
