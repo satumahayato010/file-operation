fe8eb89a0f025e97ba8a7dc141da8d7a9d83bb64
stm33 <satuma33@icloud.com>
2022-09-16 08:26:10 +0900

    [add]refs #14 file-reader

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"io"
+	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
+	"strings"
 )
 
 func main() {
-	file, err := os.OpenFile("test.txt", os.O_RDWR|os.O_APPEND, 0775)
+	read := strings.NewReader("Hello World!!!")
+	b, err := ioutil.ReadAll(read)
 	if err != nil {
 		log.Fatal(err)
 	}
-	w := []byte("write test\n")
-	_, err = file.Write(w)
-	if err != io.EOF && err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(string(b))
 }
