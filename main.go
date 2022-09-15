7504cdde9e9f54ffea2102bc8c3d3fd495ceea46
stm33 <satuma33@icloud.com>
2022-09-15 23:28:05 +0900

    [add]refs #10 file-io

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	err := os.Rename("test.txt", "test1.txt")
+	file, err := os.Open("test.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
+
+	buf := make([]byte, 64)
+	n, err := file.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(buf[:n]))
 }
